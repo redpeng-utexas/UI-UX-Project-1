9d0d6b5d249e877cc01e6b8d0165a754155bbc31
redpeng-utexas <redpeng@utexas.edu>
2026-02-02 01:22:38 -0600

    Progress so far

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let canvas;
+let bgimage;
+let bgimageratio;
+let alucardidle;
+let altest;
+
+function setup() {
+  canvas = createCanvas(window.innerWidth, window.innerHeight-60); //creates a canvas which conforms to the width and height of the website.
+  background(220);
+  bgimage = loadImage('/Images/Long Library 2.png');
+  alucardidle = Image[1];
+  for (i = 0; i > 0; i++){
+    alucardidle[i] = loadImage('/Images/Alucard Idle/AlucardIdle_' + i + '.png');
+  }
+  altest = loadImage('/Images/Alucard Idle/AlucardIdle_0.png')
+}
+
+function draw() {
+  //set bg color to background of library
+  background(144, 112, 1);
+  bgimageratio = (bgimage.height / bgimage.width);
+  image(bgimage, 0, innerHeight / 5, window.innerWidth, ( bgimageratio * innerWidth));
+  image(altest, mouseX, mouseY, altest.width, altest.height);
+}

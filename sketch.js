913cfb7a1eadaa239216c87eacba36a8cecf7a3f
redpeng-utexas <redpeng@utexas.edu>
2026-02-04 00:14:40 -0600

    More fixes...

--- sketch.js
+++ sketch.js
@@ -3,15 +3,20 @@ let dracula;
 let scaleratio;
 let draculaWidth;
 let draculaHeight;
+let draArr;
 let font;
+let frame;
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight*.96); //creates a canvas which conforms to the width and height of the website.
   background(16, 16, 16);
 
-  //dracula = []
+  draArr = [];
+  for(i = 0; i <= 25; i++){
+    draArr[i] = loadImage('/Images/Dracula/DraculaThrow_' + encodeURIComponent(i) + encodeURIComponent('.png'));
+  }
 
-  dracula = loadImage('/Images/DraculaAnimsFix/DraculaThrow_19.png');
+  dracula = loadImage('/Images/Dracula/DraculaThrow_19.png');
   draculaWidth = 200;
 
   font = loadFont('/Fonts/SymphonyoftheNightfont.ttf'); /*font and text stuff*/
@@ -20,6 +25,12 @@ function setup() {
   textFont(font);
   textSize(32);
   strokeWeight(4);
+
+
+
+  //starts the frame count
+  frame = 0;
+  setInterval(function(){frame++;}, 17);
 }
 
 function draw() {
@@ -27,9 +38,9 @@ function draw() {
   
   draculaHeight = draculaWidth * dracula.height / dracula.width; /*dracula height calculation*/
 
-  image(dracula, 1, -400, draculaWidth, draculaHeight);
+  image(draArr[15], 0, -400, draculaWidth, draculaHeight);
   
+  //parseInt(frame / 15)
   
-  
-  text(draculaHeight, 50, 50);
+  text(frame, 50, 50);
 }
